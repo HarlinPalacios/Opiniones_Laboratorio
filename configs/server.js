80627b79991ee3915e220a19cd6aba02ec6754e4
palacios <hpalacios-2020586@kinal.edu.gt>
2025-02-21 11:22:36 -0600

    creacion del usuario

--- configs/server.js
+++ configs/server.js
@@ -4,7 +4,10 @@ import express from "express"
 import cors from "cors"
 import helmet from "helmet"
 import morgan from "morgan"
-import { dbConnection } from './mongo.js'
+import { dbConnection } from "./mongo.js"
+import authRoutes from "../src/auth/auth.routes.js"
+import userRoutes from "../src/user/user.routes.js"
+import apiLimiter from "../src/middlewares/rate-limit-validator.js"
 
 const middlewares = (app) => {
     app.use(express.urlencoded({extended: false}))
@@ -12,14 +15,20 @@ const middlewares = (app) => {
     app.use(cors())
     app.use(helmet())
     app.use(morgan("dev"))
-    
+    app.use(apiLimiter)
 }
 
-const conectarDB = async () => {
+const routes = (app) =>{
+    app.use("/opiniones/v1/auth", authRoutes)
+    app.use("/opiniones/v1/user", userRoutes)
+}
+
+const conectarDB = async () =>{
     try{
         await dbConnection()
     }catch(err){
         console.log(`Database connection failed: ${err}`)
+        process.exit(1)
     }
 }
 
@@ -28,8 +37,11 @@ export const initServer = () => {
     try{
         middlewares(app)
         conectarDB()
-        console.log(`Server running on port: ${process.env.PORT}`)
+        routes(app)
+        app.listen(process.env.PORT)
+        console.log(`Server running on port ${process.env.PORT}`)
     }catch(err){
         console.log(`Server init failed: ${err}`)
     }
 }
+
